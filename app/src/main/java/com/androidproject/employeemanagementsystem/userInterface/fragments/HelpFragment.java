03b72ecebaad97d9d04834f675f395f1cf31a21c
sonia64606 <sonia64606@gmail.com>
2018-06-30 01:58:09 -0400

    Committing tab bar

---
+++ app/src/main/java/com/androidproject/employeemanagementsystem/userInterface/fragments/HelpFragment.java
@@ -0,0 +1,18 @@
+package com.androidproject.employeemanagementsystem.userInterface.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.androidproject.employeemanagementsystem.R;
+
+public class HelpFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_help, container, false);
+        return rootView;
+    }
+}
