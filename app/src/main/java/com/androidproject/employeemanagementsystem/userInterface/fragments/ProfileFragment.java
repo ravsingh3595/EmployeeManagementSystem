5b0ed84ea11a9471121b01005a89138a70766128
sonia64606 <sonia64606@gmail.com>
2018-07-05 13:23:17 -0400

    Committing code after working on profile - open camera and gallery - open map - open calendar

--- app/src/main/java/com/androidproject/employeemanagementsystem/userInterface/fragments/ProfileFragment.java
+++ app/src/main/java/com/androidproject/employeemanagementsystem/userInterface/fragments/ProfileFragment.java
@@ -1,105 +1,261 @@
 package com.androidproject.employeemanagementsystem.userInterface.fragments;
 
-import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.icu.util.Calendar;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.androidproject.employeemanagementsystem.R;
-//import com.google.android.gms.maps.CameraUpdateFactory;
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.MapView;
-//import com.google.android.gms.maps.MapsInitializer;
-//import com.google.android.gms.maps.OnMapReadyCallback;
-//import com.google.android.gms.maps.model.CameraPosition;
-//import com.google.android.gms.maps.model.LatLng;
-//import com.google.android.gms.maps.model.MarkerOptions;
+import com.androidproject.employeemanagementsystem.util.pdf.Utility;
 
-public class ProfileFragment extends Fragment{
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-//    MapView mMapView;
-//    private GoogleMap googleMap;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+
+public class ProfileFragment extends Fragment {
+
+    @BindView(R.id.imgProfile)
+    ImageView imgProfile;
+    @BindView(R.id.edtName)
+    EditText edtName;
+    @BindView(R.id.layoutName)
+    TextInputLayout layoutName;
+    @BindView(R.id.edtDOB)
+    EditText edtDOB;
+    @BindView(R.id.layoutDOB)
+    TextInputLayout layoutDOB;
+    @BindView(R.id.btnSelectDate)
+    Button btnSelectDate;
+    @BindView(R.id.linearDob)
+    LinearLayout linearDob;
+    @BindView(R.id.tvGender)
+    TextView tvGender;
+    @BindView(R.id.radioButton2)
+    RadioButton radioButton2;
+    @BindView(R.id.radioButton)
+    RadioButton radioButton;
+    @BindView(R.id.rgGender)
+    RadioGroup rgGender;
+    @BindView(R.id.edtAddress)
+    EditText edtAddress;
+    @BindView(R.id.input_layout_address)
+    TextInputLayout inputLayoutAddress;
+    @BindView(R.id.btnSave)
+    Button btnSave;
+    @BindView(R.id.btnUpload)
+    Button btnUpload;
+    @BindView(R.id.constraintLayout)
+    RelativeLayout constraintLayout;
+    Unbinder unbinder;
+    @BindView(R.id.btnShowLoc)
+    Button btnShowLoc;
+
+    private DatePickerDialog datePickerDialog;
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private String userChoosenTask;
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
-/*
-        mMapView = (MapView) rootView.findViewById(R.id.mapView);
-        mMapView.onCreate(savedInstanceState);
 
-        mMapView.onResume(); // needed to get the map to display immediately
+        unbinder = ButterKnife.bind(this, rootView);
 
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        mMapView.getMapAsync(new OnMapReadyCallback() {
-                                 @Override
-                                 public void onMapReady(GoogleMap googleMap) {
-
-
-                                     // For showing a move to my location button
-
-
-                                     if (ContextCompat.checkSelfPermission(container.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                                             == PackageManager.PERMISSION_GRANTED) {
-                                         googleMap.setMyLocationEnabled(true);
-                                     } else {
-                                         // Show rationale and request permission.
-                                     }
-
-                                     // For dropping a marker at a point on the Map
-                                     LatLng sydney = new LatLng(-34, 151);
-                                     googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                                     // For zooming automatically to the location of the marker
-                                     CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                                     googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                                 }
-                             }
-
-        );*/
         return rootView;
     }
 
-    /*
+    @OnClick({R.id.btnSelectDate, R.id.btnSave, R.id.btnUpload})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnSelectDate:
+                selectDateButtonTapped();
+                break;
+            case R.id.btnSave:
+                break;
+            case R.id.btnUpload:
+                selectImage();
+                break;
+        }
+    }
+
+    public void selectDateButtonTapped() {
+
+        // calender class's instance and get current date , month and year from calender
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR); // current year
+        int mMonth = c.get(Calendar.MONTH); // current month
+        int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+
+        datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // set day of month , month and year value in the edit text
+                edtDOB.setText(day + "/"
+                        + (month + 1) + "/" + year);
+            }
+        }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (userChoosenTask.equals("Take Photo"))
+                        cameraIntent();
+                    else if (userChoosenTask.equals("Choose from Library"))
+                        galleryIntent();
+                } else {
+                    //code for deny
+                }
+                break;
+        }
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = {"Take Photo", "Choose from Library",
+                "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(getActivity());
+
+                if (items[item].equals("Take Photo")) {
+                    userChoosenTask = "Take Photo";
+                    if (result)
+                        cameraIntent();
+
+                } else if (items[item].equals("Choose from Library")) {
+                    userChoosenTask = "Choose from Library";
+                    if (result)
+                        galleryIntent();
+
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void galleryIntent() {
+        Log.e("Gellery", "Gellery INtent called");
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    private void cameraIntent() {
+        Log.e("Camera", "Camera INtent called");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mMapView.onResume();
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mMapView.onPause();
+    private void onCaptureImageResult(Intent data) {
+        Log.e("onCaptureImageResult", "onCaptureImageResult called");
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imgProfile.setImageBitmap(thumbnail);
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mMapView.onDestroy();
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Log.e("onSelectFromGalleryResult ", "onSelectFromGalleryResult called");
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        imgProfile.setImageBitmap(bm);
     }
 
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
+    @OnClick(R.id.btnShowLoc)
+    public void onViewClicked() {
+        String url = "geo:0,0?q=Brooklyn+Bridge,New+York,NY";
+
+        Uri uri = Uri.parse(url);
+        showMap(uri);
     }
-    */
+
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 }
 
